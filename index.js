f8bf49e7bf3e9133183d5b54d23967bdbf33c493
Lurgrid <Lurgrid@gmail.com>
2022-08-22 15:34:30 +0200

    0.0.3

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import config from './Configuration/CreateBot.json' assert {type: 'json'};
 import pkg from 'mineflayer-pathfinder';
 const { pathfinder } = pkg;
 
+import rl from 'serverline'
+
 
 export class Client{
     constructor(){
@@ -14,13 +16,21 @@ export class Client{
             password: config.password,
             auth: config.auth
         })
-        this.commands = new Map();
         this.bot.loadPlugin(pathfinder)
+        
+        this.rl = rl
+        this.rl.init()
+        this.commands = new Map();
+        process.stdout.write('\x1Bc')
     }
 
     async execute (){
         (await import("./Handlers/Commands.js")).default(this);
         (await import("./Handlers/Events.js")).default(this);
+
+        this.rl.on('line', string => {
+            this.bot.chat(string)
+        })
     }
 };
 new Client().execute();
