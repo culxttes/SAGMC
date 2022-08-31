cc180b6ee4a758eb5efc3784ac18bf63f83f4a2e
Lurgrid <Lurgrid@gmail.com>
2022-09-01 00:30:59 +0200

    22Y09M01

--- Commands/Test/test.js
+++ Commands/Test/test.js
@@ -8,8 +8,12 @@ export default{
     * @param {Client} client
     */
     async execute(args, username, message, client) {
-        client.bot._client.once('player_info', (packet) => {
+        /*client.bot._client.once('player_info', (packet) => {
             console.log(packet)
+            console.log(packet.data[0].name)
+        })*/
+        client.bot._client.on("tab_complete", data => {
+            console.log(data)
         })
     }
 }
